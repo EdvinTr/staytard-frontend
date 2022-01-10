0f6af560e1a08ebdfe21cd9ce541dac0026d7cc4
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-10 21:04:59 +0100

    fix: popover fade y

--- src/components/navbar/my-pages-popover/MyPagesPopover.tsx
+++ src/components/navbar/my-pages-popover/MyPagesPopover.tsx
@@ -47,24 +47,24 @@ export const MyPagesPopover = ({ ...props }: MyPagesPopoverProps) => {
             <Transition
               as={Fragment}
               enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
+              enterFrom="opacity-0 translate-y-4"
               enterTo="opacity-100 translate-y-0"
               leave="transition ease-in duration-150"
               leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
+              leaveTo="opacity-0 translate-y-4"
             >
-              <Popover.Panel className="absolute z-10 w-80 max-w-sm  px-2 mt-8 transform -translate-x-1/2 -left-14   sm:px-0 lg:max-w-3xl">
-                <div className="overflow-hidden  shadow-lg ">
+              <Popover.Panel className="absolute z-10 w-80 max-w-sm px-2 mt-8 transform -translate-x-1/2 -left-14 sm:px-0 lg:max-w-3xl">
+                <div className="overflow-hidden shadow-lg ">
                   <div className="relative grid gap-8 bg-white px-4 py-8 border-l-black border-r-black border-b-black border-opacity-5  ">
                     {solutions.map((item) => (
                       <Link href={item.href} key={item.name}>
-                        <a className="flex items-center justify-between  p-2 -m-3 transition duration-150 ease-in-out  hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-staytard-yellow focus-visible:ring-opacity-50">
+                        <a className="flex items-center justify-between p-2 -m-3 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-staytard-yellow focus-visible:ring-opacity-50">
                           <div className="ml-4">
                             <p className="text-base text-staytard-dark">
                               {item.name}
                             </p>
                           </div>
-                          <div className="flex items-center justify-center  w-10 h-10 text-white sm:h-12 sm:w-12">
+                          <div className="flex items-center justify-center w-10 h-10 text-white sm:h-12 sm:w-12">
                             <item.icon aria-hidden="true" className="w-8" />
                           </div>
                         </a>
