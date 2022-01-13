e588f43704a6dbec75848d2b23615b1d48f6382c
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-13 19:05:22 +0100

    feat: reusable accordion

--- src/components/footer/Accordion.tsx
+++ src/components/footer/Accordion.tsx
@@ -2,10 +2,12 @@ import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 
-export const Accordion: React.FC<{}> & {
+interface AccordionProps {
+  title: string;
+}
+export const Accordion: React.FC<AccordionProps> & {
   Body: typeof Body;
-  Header: typeof Header;
-} = ({ children }) => {
+} = ({ children, title }) => {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   return (
@@ -18,7 +20,7 @@ export const Accordion: React.FC<{}> & {
           className="flex justify-between w-full py-5 px-8 "
           onClick={() => setIsAccordionOpen(!isAccordionOpen)}
         >
-          <div className="text-base font-bold">Shop safe</div>
+          <div className="text-base font-bold">{title}</div>
           {isAccordionOpen ? (
             <ChevronUpIcon
               className={`w-6 transition-all duration-[400ms] ease-out ${
@@ -60,8 +62,6 @@ export const Accordion: React.FC<{}> & {
   );
 };
 
-const Header: React.FC = ({ children }): JSX.Element => <>{children}</>;
 const Body: React.FC = ({ children }): JSX.Element => <>{children}</>;
 
 Accordion.Body = Body;
-Accordion.Header = Header;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,36 +1,60 @@
 import React, { Fragment } from "react";
+import { APP_NAME } from "../../constants";
 import { Accordion } from "./Accordion";
 interface FooterProps {}
 
+interface FooterItem {
+  title: string;
+  listItems: string[];
+}
+const footerItems: FooterItem[] = [
+  {
+    title: "Shop Safe",
+    listItems: [
+      "Returns",
+      "Terms and Conditions",
+      "Deliveries",
+      "Personal data policy",
+    ],
+  },
+  {
+    title: "Information",
+    listItems: [
+      `About ${APP_NAME}`,
+      "Sustainability",
+      "Cookies",
+      "Affiliate",
+      `Work at ${APP_NAME}`,
+      `#yes${APP_NAME.toLowerCase()}`,
+      "About Ellos Group",
+    ],
+  },
+  {
+    title: "Gift Cards",
+    listItems: ["Buy gift cards"],
+  },
+  {
+    title: "Our friends",
+    listItems: ["Elpy", "Ellos", "Jotex", "Homeroom"],
+  },
+];
 export const Footer: React.FC<FooterProps> = ({}) => {
   return (
     <Fragment>
       <div className="h-screen bg-staytard-dark text-white  ">
         {/* accordion button */}
         <div className="h-10"></div>
-        <Accordion>
-          <Accordion.Body>
-            <ul className="space-y-5 px-16 pb-6">
-              <li>Returns</li>
-              <li>Terms and Conditions</li>
-              <li>Deliveries</li>
-              <li>Personal data policy</li>
-            </ul>
-          </Accordion.Body>
-        </Accordion>
-        <Accordion>
-          <Accordion.Header>
-            <div>Hello world</div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <ul className="space-y-5 px-16 pb-6">
-              <li>Returns</li>
-              <li>Terms and Conditions</li>
-              <li>Deliveries</li>
-              <li>Personal data policy</li>
-            </ul>
-          </Accordion.Body>
-        </Accordion>
+        {footerItems.map(({ listItems, title }, idx) => (
+          <Accordion title={title} key={idx}>
+            <Accordion.Body>
+              <ul className="space-y-5 px-16 pb-6">
+                {listItems.map((item, idx) => (
+                  <li key={idx}>{item}</li>
+                ))}
+              </ul>
+            </Accordion.Body>
+          </Accordion>
+        ))}
       </div>
     </Fragment>
   );
