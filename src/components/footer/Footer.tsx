12bc299e19bd9d52401a76a8d49fbb1af10eb4c9
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-13 19:47:51 +0100

    fix: border on last accordion item

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,18 +9,27 @@ export const Footer: React.FC<FooterProps> = ({}) => {
       <div className="bg-staytard-dark text-white  ">
         {/* accordion button */}
         <div className="h-10"></div>
-        {footerItems.map(({ listItems, title }, idx) => (
-          <Accordion title={title} key={idx}>
-            <Accordion.Body>
-              <ul className="space-y-5 px-16 pb-6">
-                {listItems.map((item, idx) => (
-                  <li key={idx}>{item}</li>
-                ))}
-              </ul>
-            </Accordion.Body>
-          </Accordion>
-        ))}
-        <div className="flex items-center justify-center space-x-8 pb-12 pt-32 min-h-[10rem]">
+        <div>
+          {footerItems.map(({ listItems, title }, idx, arr) => {
+            const isLastItem = idx === arr.length - 1;
+            return (
+              <Accordion
+                title={title}
+                key={idx}
+                className={`${isLastItem && "border-b border-opacity-10"}`}
+              >
+                <Accordion.Body>
+                  <ul className="space-y-5 px-16 pb-6">
+                    {listItems.map((item, idx) => (
+                      <li key={idx}>{item}</li>
+                    ))}
+                  </ul>
+                </Accordion.Body>
+              </Accordion>
+            );
+          })}
+        </div>
+        <div className="flex items-center justify-center space-x-12 pb-12 pt-32 min-h-[10rem]">
           <InstagramIcon />
           <FacebookIcon />
           <TikTokIcon />
@@ -40,7 +49,7 @@ function FacebookIcon() {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      className="fill-current text-white"
+      className="fill-current text-white w-6"
     >
       <path d="M17.525,9H14V7c0-1.032,0.084-1.682,1.563-1.682h1.868v-3.18C16.522,2.044,15.608,1.998,14.693,2 C11.98,2,10,3.657,10,6.699V9H7v4l3-0.001V22h4v-9.003l3.066-0.001L17.525,9z"></path>
     </svg>
@@ -56,7 +65,7 @@ function TikTokIcon() {
       width="30"
       height="30"
       viewBox="0 0 30 30"
-      className="fill-current text-white"
+      className="fill-current text-white w-6"
     >
       <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.104,4,24,4z M22.689,13.474 c-0.13,0.012-0.261,0.02-0.393,0.02c-1.495,0-2.809-0.768-3.574-1.931c0,3.049,0,6.519,0,6.577c0,2.685-2.177,4.861-4.861,4.861 C11.177,23,9,20.823,9,18.139c0-2.685,2.177-4.861,4.861-4.861c0.102,0,0.201,0.009,0.3,0.015v2.396c-0.1-0.012-0.197-0.03-0.3-0.03 c-1.37,0-2.481,1.111-2.481,2.481s1.11,2.481,2.481,2.481c1.371,0,2.581-1.08,2.581-2.45c0-0.055,0.024-11.17,0.024-11.17h2.289 c0.215,2.047,1.868,3.663,3.934,3.811V13.474z"></path>
     </svg>
@@ -71,7 +80,7 @@ function YouTubeIcon() {
       width="30"
       height="30"
       viewBox="0 0 30 30"
-      className="fill-current text-white"
+      className="fill-current text-white w-6"
     >
       <path d="M 15 4 C 10.814 4 5.3808594 5.0488281 5.3808594 5.0488281 L 5.3671875 5.0644531 C 3.4606632 5.3693645 2 7.0076245 2 9 L 2 15 L 2 15.001953 L 2 21 L 2 21.001953 A 4 4 0 0 0 5.3769531 24.945312 L 5.3808594 24.951172 C 5.3808594 24.951172 10.814 26.001953 15 26.001953 C 19.186 26.001953 24.619141 24.951172 24.619141 24.951172 L 24.621094 24.949219 A 4 4 0 0 0 28 21.001953 L 28 21 L 28 15.001953 L 28 15 L 28 9 A 4 4 0 0 0 24.623047 5.0546875 L 24.619141 5.0488281 C 24.619141 5.0488281 19.186 4 15 4 z M 12 10.398438 L 20 15 L 12 19.601562 L 12 10.398438 z"></path>
     </svg>
@@ -87,7 +96,7 @@ function InstagramIcon() {
       width="32"
       height="32"
       viewBox="0 0 32 32"
-      className="fill-current text-white"
+      className="fill-current text-white w-6"
     >
       <path d="M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z"></path>
     </svg>
