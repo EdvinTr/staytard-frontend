02efa9c34670cdeedcc2c078abc2d2f258246d6a
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-13 20:03:54 +0100

    fix: hide accordions on large screen

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { ChevronRightIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 import React, { Fragment } from "react";
+import { useWindowSize } from "../../hooks/useWindowSize";
 import { Accordion } from "../global/Accordion";
 import {
   FacebookIcon,
@@ -12,6 +13,7 @@ import { footerItems } from "./footer-items.data";
 interface FooterProps {}
 
 export const Footer: React.FC<FooterProps> = ({}) => {
+  const { width: windowWidth } = useWindowSize();
   return (
     <Fragment>
       <div className="bg-staytard-dark text-white">
@@ -27,27 +29,29 @@ export const Footer: React.FC<FooterProps> = ({}) => {
             </a>
           </Link>
         </article>
-        {/* footer accordions */}
-        <div>
-          {footerItems.map(({ listItems, title }, idx, arr) => {
-            const isLastItem = idx === arr.length - 1;
-            return (
-              <Accordion
-                title={title}
-                key={idx}
-                className={`${isLastItem && "border-b border-opacity-10"}`}
-              >
-                <Accordion.Body>
-                  <ul className="space-y-5 px-16 pb-6">
-                    {listItems.map((item, idx) => (
-                      <li key={idx}>{item}</li>
-                    ))}
-                  </ul>
-                </Accordion.Body>
-              </Accordion>
-            );
-          })}
-        </div>
+        {windowWidth && windowWidth <= 1024 && (
+          /* accordions */
+          <div>
+            {footerItems.map(({ listItems, title }, idx, arr) => {
+              const isLastItem = idx === arr.length - 1;
+              return (
+                <Accordion
+                  title={title}
+                  key={idx}
+                  className={`${isLastItem && "border-b border-opacity-10"}`}
+                >
+                  <Accordion.Body>
+                    <ul className="space-y-5 px-16 pb-6">
+                      {listItems.map((item, idx) => (
+                        <li key={idx}>{item}</li>
+                      ))}
+                    </ul>
+                  </Accordion.Body>
+                </Accordion>
+              );
+            })}
+          </div>
+        )}
         {/* social media icons */}
         <div className="flex items-center justify-center space-x-12 pb-12 pt-32 min-h-[10rem]">
           <InstagramIcon />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,22 @@ const IndexPage: NextPage = () => {
   return (
     <FadeInContainer className="text-stayhard-dark">
       <div className=" text-staytard-dark">
-        <div className="flex items-center flex-wrap gap-8 mt-20">
+        <div className="flex items-center justify-center py-12 flex-wrap gap-8 mt-20">
           <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-red-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-green-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-green-500"></div>
+          <div className="w-72 h-72 bg-purple-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-orange-500"></div>
+          <div className="w-72 h-72 bg-green-500"></div>
+          <div className="w-72 h-72 bg-blue-500"></div>
         </div>
       </div>
     </FadeInContainer>
