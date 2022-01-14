492c1fa0254f1478cf8a75db851def0c7958e057
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-14 12:42:50 +0100

    fix: footer grid

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -20,54 +20,57 @@ export const Footer: React.FC<FooterProps> = ({}) => {
   renders++;
   console.log(renders);
   const [windowWidth] = useSsrCompatible(useWindowSize(), [0, 0]);
-  const imageSize = windowWidth >= 768 ? 24 : 48;
+  const imageSize = windowWidth >= 768 ? 30 : 48;
 
   return (
     <Fragment>
       <div className="bg-staytard-dark text-white">
         {/* customer service link */}
-        <section className="md:flex md:justify-center md:items-center md:space-x-28 space-y-8 md:space-y-0 py-12 text-center text-xl md:text-sm text-staytard-dark bg-[#ebebeb] ">
-          {/* free shipping */}
-          <div className="">
-            <Image
-              width={imageSize}
-              height={imageSize}
-              objectFit="contain"
-              src="https://img.icons8.com/material-outlined/48/000000/truck.png"
-              alt="Free shipping"
-            />
-            <div className="underline font-semibold">Free shipping*</div>
-            <span className="text-lg md:text-xs">Orders over SEK 499</span>
-          </div>
-          <div>
-            <Image
-              width={imageSize}
-              height={imageSize}
-              objectFit="contain"
-              src="https://img.icons8.com/glyph-neue/64/000000/forward-30.png"
-              alt="30 days return policy"
-            />
-            <div className="underline font-semibold">Easy return*</div>
-            <span className="text-lg md:text-xs">30 days return policy</span>
-          </div>
-          <div>
-            <Image
-              width={imageSize}
-              height={imageSize}
-              objectFit="contain"
-              src="https://img.icons8.com/ios-filled/50/000000/coconut-cocktail.png"
-              alt="Pay later"
-            />
-            <div className="underline font-semibold">Pay later</div>
-            <span className="text-lg md:text-xs">
-              Select invoice at checkout
-            </span>
+        <section className="text-staytard-dark bg-[#ebebeb] ">
+          <div className="md:grid md:grid-cols-3 max-w-3xl mx-auto space-y-8 md:space-y-0 py-12 text-center text-xl md:text-sm ">
+            {/* free shipping */}
+            <div className="">
+              <Image
+                width={imageSize}
+                height={imageSize}
+                objectFit="contain"
+                src="https://img.icons8.com/material-outlined/48/000000/truck.png"
+                alt="Free shipping"
+              />
+              <div className="underline font-semibold">Free shipping*</div>
+              <span className="text-lg md:text-xs">Orders over SEK 499</span>
+            </div>
+            <div>
+              <Image
+                width={imageSize}
+                height={imageSize}
+                objectFit="contain"
+                src="https://img.icons8.com/glyph-neue/64/000000/forward-30.png"
+                alt="30 days return policy"
+              />
+              <div className="underline font-semibold">Easy return*</div>
+              <span className="text-lg md:text-xs">30 days return policy</span>
+            </div>
+            <div>
+              <Image
+                width={imageSize}
+                height={imageSize}
+                objectFit="contain"
+                src="https://img.icons8.com/ios-filled/50/000000/coconut-cocktail.png"
+                alt="Pay later"
+              />
+              <div className="underline font-semibold">Pay later</div>
+              <span className="text-lg md:text-xs">
+                Select invoice at checkout
+              </span>
+            </div>
           </div>
         </section>
+        {/* customer service */}
         <article className="text-center font-bold space-y-6 pb-14 pt-10">
           <p>Need help?</p>
           <Link href="#">
-            <a className="bg-white font-semibold text-staytard-dark inline-block text-13 px-12 py-4">
+            <a className="bg-white font-semibold text-staytard-dark inline-block text-13 px-8 py-4">
               <div className="flex items-center">
                 <span>Customer service</span>
                 <ChevronRightIcon className="w-5" />
@@ -99,25 +102,30 @@ export const Footer: React.FC<FooterProps> = ({}) => {
           </div>
         ) : (
           /* large device grid */
-          <div className="container mx-auto">
-            <ul className="grid grid-cols-4 px-12 content-center ">
-              {footerItems.map(({ title, listItems }, idx) => {
-                return (
-                  <div key={idx}>
-                    <p className="font-bold">{title}</p>
-                    <div className="space-y-3 pt-5">
-                      {listItems.map((listItem, idx) => {
-                        return (
-                          <li className="text-sm text-[#bdbec1]" key={idx}>
-                            {listItem}
-                          </li>
-                        );
-                      })}
+          <div className="flex justify-center">
+            <div>
+              <ul className="grid grid-cols-4 px-12 gap-x-32">
+                {footerItems.map(({ title, listItems }, idx) => {
+                  return (
+                    <div key={idx}>
+                      <p className="font-bold">{title}</p>
+                      <div className="space-y-3 pt-5">
+                        {listItems.map((listItem, idx) => {
+                          return (
+                            <li
+                              className="text-sm text-[#bdbec1] hover:underline"
+                              key={idx}
+                            >
+                              {listItem}
+                            </li>
+                          );
+                        })}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </ul>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         )}
         {/* social media icons */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
 import { NextPage } from "next";
 import { FadeInContainer } from "../components/global/FadeInContainer";
+import { MyContainer } from "../components/MyContainer";
 const IndexPage: NextPage = () => {
   return (
     <FadeInContainer className="text-stayhard-dark">
       <div className=" text-staytard-dark">
-        <div className="flex items-center justify-center py-12 flex-wrap gap-8 mt-20">
-          <div className="w-72 h-72 bg-red-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-green-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-green-500"></div>
-          <div className="w-72 h-72 bg-purple-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-orange-500"></div>
-          <div className="w-72 h-72 bg-green-500"></div>
-          <div className="w-72 h-72 bg-blue-500"></div>
-        </div>
+        <MyContainer>
+          <div className="flex items-center justify-center py-12 flex-wrap gap-8 mt-20">
+            <div className="w-72 h-72 bg-red-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-green-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-green-500"></div>
+            <div className="w-72 h-72 bg-purple-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-orange-500"></div>
+            <div className="w-72 h-72 bg-green-500"></div>
+            <div className="w-72 h-72 bg-blue-500"></div>
+            <div className="w-72 h-72 bg-blue-500"></div>
+          </div>
+        </MyContainer>
       </div>
     </FadeInContainer>
   );
