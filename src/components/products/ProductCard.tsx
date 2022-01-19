dc9dc0ef49a474b9a65d8b92a64368bcee6e0fc9
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-19 21:04:47 +0100

    feat: cache images for faster load

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import NextImage from "next/image";
 import React, { Fragment, useCallback, useState } from "react";
 import { FindProductsQuery } from "../../lib/graphql";
 interface ProductCardProps {
@@ -10,22 +10,33 @@ let renders = 0;
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   renders++;
   console.log(renders);
-
+  const slicedImages = product.images.slice(0, 4);
   const [isHovered, setIsHovered] = useState(false);
 
   const [activeImage, setActiveImage] = useState(
-    product.images[0].imageUrl.replace("{size}", largeImageSize)
+    slicedImages[0].imageUrl.replace("{size}", largeImageSize)
   );
 
-  const onMouseEnter = useCallback(() => {
+  const cacheImages = async () => {
+    const promises = slicedImages.map(({ imageUrl }) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image();
+        img.src = imageUrl.replace("{size}", largeImageSize);
+        img.onload = () => resolve(img.height);
+        img.onerror = reject;
+      });
+    });
+    await Promise.all(promises);
+  };
+
+  const onMouseEnter = useCallback(async () => {
     setIsHovered(true);
-    for (const image of product.images) {
-      fetch(image.imageUrl.replace("{size}", largeImageSize));
-      fetch(image.imageUrl.replace("{size}", smallImageSize));
-    }
+    setActiveImage(slicedImages[1].imageUrl.replace("{size}", largeImageSize));
+    await cacheImages();
   }, [setIsHovered]);
 
   const onMouseLeave = useCallback(() => {
+    setActiveImage(slicedImages[0].imageUrl.replace("{size}", largeImageSize));
     setIsHovered(false);
   }, [setIsHovered]);
 
@@ -34,7 +45,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       className=" p-3 hover:shadow-md hover:transition-shadow hover:duration-300 hover:ease-in-out"
       onMouseLeave={onMouseLeave}
     >
-      <Image
+      <NextImage
         src={activeImage}
         placeholder="blur"
         priority
@@ -47,14 +58,14 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       />
       {isHovered ? (
         <div className="flex items-center space-x-2">
-          {product.images.map((image, idx) => {
+          {slicedImages.map((image, idx) => {
             const smallImageUrl = image.imageUrl.replace(
               "{size}",
               smallImageSize
             );
             return (
               <div key={idx}>
-                <Image
+                <NextImage
                   src={smallImageUrl}
                   placeholder="blur"
                   blurDataURL={smallImageUrl}
