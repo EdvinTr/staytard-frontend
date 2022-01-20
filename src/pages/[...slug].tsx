84bc999beb79ea9379bcfd6d6daeff53d7e98fc6
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-01-20 14:46:19 +0100

    fix: show loading indicator when loading more

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,6 +1,8 @@
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
 import { FadeInContainer } from "../components/global/FadeInContainer";
 import { MyContainer } from "../components/MyContainer";
 import { ProductCard } from "../components/products/ProductCard";
@@ -12,7 +14,8 @@ const getFullPath = (slug: string[]) => {
   return fullUrl;
 };
 const SlugPage: NextPage = (props) => {
-  const { data, fetchMore, loading } = ssrFindProducts.usePage();
+  const { data, fetchMore } = ssrFindProducts.usePage();
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const fullPath = getFullPath(router.query.slug as string[]);
 
@@ -24,6 +27,23 @@ const SlugPage: NextPage = (props) => {
       </FadeInContainer>
     );
   }
+  const onFetchMore = async () => {
+    try {
+      setIsLoading(true);
+      await fetchMore({
+        variables: {
+          input: {
+            offset: data.products.items.length,
+            limit: 50,
+            categoryPath: fullPath,
+          },
+        },
+      });
+    } catch {
+    } finally {
+      setIsLoading(false);
+    }
+  };
   const { hasMore, items, totalCount } = data.products;
   return (
     <FadeInContainer className="text-stayhard-dark min-h-screen pb-40 relative">
@@ -37,8 +57,16 @@ const SlugPage: NextPage = (props) => {
         </div>
       </MyContainer>
       {/* load more group */}
-      <div className="pt-8 max-w-xs mx-auto space-y-4">
-        <div className="px-6 space-y-1">
+      <div className="pt-8 max-w-xs mx-auto space-y-4 relative">
+        {isLoading && (
+          <div className="absolute inset-0">
+            <BeatLoader
+              color="#faba"
+              css="display:flex; justify-content:center;"
+            />
+          </div>
+        )}
+        <div className="px-2 space-y-1 text-center">
           <p className="text-[#6b6b6b]">
             You have seen {items.length} of {totalCount} products
           </p>
@@ -57,15 +85,7 @@ const SlugPage: NextPage = (props) => {
           <button
             className="text-white w-full bg-staytard-dark p-4 flex justify-center items-center"
             onClick={async () => {
-              await fetchMore({
-                variables: {
-                  input: {
-                    offset: data.products.items.length,
-                    limit: 50,
-                    categoryPath: fullPath,
-                  },
-                },
-              });
+              await onFetchMore();
             }}
           >
             <span>Show more</span>
