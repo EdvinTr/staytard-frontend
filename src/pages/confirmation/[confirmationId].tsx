d725beda9e28e618a91ac25ab1cce44d4f62afc5
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-02 22:34:55 +0100

    chore: add todo comments

--- src/pages/confirmation/[confirmationId].tsx
+++ src/pages/confirmation/[confirmationId].tsx
@@ -4,11 +4,15 @@ import { FadeInContainer } from "../../components/global/FadeInContainer";
 const ConfirmationPage: NextPage = () => {
   const router = useRouter();
   const confirmationId = router.query.confirmationId as string;
+
+  //TODO: should not be able to get here unless you have a valid orderId and this order belongs to the currently logged in user
   return (
     <FadeInContainer className="text-staytard-dark min-h-screen">
-      <div className=" text-staytard-dark">
-        <h1>You get here when you confirm payment or something I guess</h1>
-        <h2>Here is the order number I guess: {confirmationId}</h2>
+      <div className=" text-staytard-dark text-center mt-20 space-y-7">
+        <h1 className="text-2xl font-bold">Thanks for the order mate!</h1>
+        <h2 className="text-xl tracking-wide">
+          <span className="font-semibold">Order number:</span> {confirmationId}
+        </h2>
       </div>
     </FadeInContainer>
   );
