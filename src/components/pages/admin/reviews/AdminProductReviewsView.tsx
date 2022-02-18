72050957e5203a76919bd22ea16cc005ec8235b3
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-18 13:49:12 +0100

    add: review cards list

--- src/components/pages/admin/reviews/AdminProductReviewsView.tsx
+++ src/components/pages/admin/reviews/AdminProductReviewsView.tsx
@@ -1,8 +1,11 @@
+import { ChevronRightIcon } from "@heroicons/react/solid";
 import React from "react";
 import { useFindAllProductReviewsQuery } from "../../../../lib/graphql";
+import { BasicCard } from "../../../global/BasicCard";
 import { CenteredBeatLoader } from "../../../global/CenteredBeatLoader";
 import { PaddingContainer } from "../components/PaddingContainer";
 import { PageHeading } from "../components/PageHeading";
+import { ItemDetailRow } from "../products/components/ProductViewRow";
 
 export const AdminProductReviewsView = () => {
   const { data, fetchMore, loading } = useFindAllProductReviewsQuery({
@@ -13,7 +16,6 @@ export const AdminProductReviewsView = () => {
       },
     },
   });
-  console.log(data);
   return (
     <div className="relative">
       <div className="bg-staytard-semi-light-gray">
@@ -23,7 +25,56 @@ export const AdminProductReviewsView = () => {
           </div>
         </PaddingContainer>
       </div>
-      <PaddingContainer>{loading && <CenteredBeatLoader />}</PaddingContainer>
+      <PaddingContainer className="text-sm">
+        {loading && <CenteredBeatLoader />}
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8 2xl:grid-cols-5">
+          {data?.allProductReviews.items.map((review) => {
+            return (
+              <BasicCard key={review.id}>
+                <article className="truncate p-4">
+                  <div className="flex items-center justify-between">
+                    <h2 className="truncate font-medium">{review.title}</h2>
+                    <ChevronRightIcon className="w-6" />
+                  </div>
+                  <div className="mt-4">
+                    <ItemDetailRow
+                      backgroundColor="gray"
+                      label="Status"
+                      value={`${
+                        review.isPublished ? "Published" : "Unpublished"
+                      }`}
+                      valueClassName={`${
+                        review.isPublished ? "text-green-600" : "text-red-600"
+                      }`}
+                    />
+                    <ItemDetailRow
+                      backgroundColor="none"
+                      label="Rating"
+                      value={`${review.rating}`}
+                    />
+                    <ItemDetailRow
+                      valueClassName="truncate"
+                      backgroundColor="gray"
+                      label="Nickname"
+                      value={`${review.nickname}`}
+                    />
+                    {review.publishedAt && (
+                      <ItemDetailRow
+                        backgroundColor="none"
+                        label="Published At"
+                        value={`${new Date(
+                          review.publishedAt
+                        ).toLocaleString()}`}
+                      />
+                    )}
+                  </div>
+                </article>
+              </BasicCard>
+            );
+          })}
+        </div>
+      </PaddingContainer>
     </div>
   );
 };
