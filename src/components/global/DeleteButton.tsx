3b529f36f5b83f8b8abd722238101ba70214784d
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-19 19:27:02 +0100

    refactor: delete button

---
+++ src/components/global/DeleteButton.tsx
@@ -0,0 +1,20 @@
+import { TrashIcon } from "@heroicons/react/solid";
+import React from "react";
+
+interface DeleteButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {}
+
+export const DeleteButton = ({ ...props }) => {
+  return (
+    <button
+      {...props}
+      type="button"
+      className="bg-red-100 p-2 text-red-600 transition-all duration-100 ease-in-out hover:bg-red-600 hover:text-white"
+    >
+      <TrashIcon className="h-4" aria-hidden />
+    </button>
+  );
+};
--- src/components/pages/admin/reviews/edit/EditProductReview.tsx
+++ src/components/pages/admin/reviews/edit/EditProductReview.tsx
@@ -1,9 +1,13 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { ProductReview } from "../../../../../lib/graphql";
+import { updateProductReviewValidationSchema } from "../../../../../utils/validation/productReviewValidationSchema";
 import { BaseButton } from "../../../../global/BaseButton";
 import { BaseInput } from "../../../../global/BaseInput";
+import { CustomTextArea } from "../../../../global/CustomTextArea";
+import { DeleteButton } from "../../../../global/DeleteButton";
 import { MyRatingSelect } from "../../../../global/MyRatingSelect";
+import { ConfirmDeletionModal } from "../../components/ConfirmDeletionModal";
 import {
   BasicInputLabel,
   DisabledInput,
@@ -17,26 +21,91 @@ interface FormValues {
   content: string;
   isPublished: boolean;
 }
+
 const inputClassNames =
   "border border-black border-opacity-10 focus:border-opacity-100 focus:outline-none focus:ring-0";
 export const EditProductReview = ({ review }: EditProductReviewProps) => {
+  const [isConfirmDeleteModalOpen, setIsConfirmDeleteModalOpen] =
+    useState(false);
   return (
     <div>
-      <div className="space-y-4 md:flex md:items-center md:justify-between md:space-x-7 md:space-y-0">
-        <div className="w-full">
-          <BasicInputLabel htmlFor="productId">Review ID</BasicInputLabel>
-          <DisabledInput name="reviewId" value={review.id.toString()} />
-        </div>
-        <div className="w-full">
-          <BasicInputLabel htmlFor="productId">Product ID</BasicInputLabel>
-          <DisabledInput name="productId" value={review.productId.toString()} />
-        </div>
+      <div className="flex items-center justify-between pb-10">
+        <h2 className="text-2xl font-medium">General information</h2>
+        <DeleteButton
+          onClick={() => setIsConfirmDeleteModalOpen(true)}
+          aria-label="Open delete review confirmation modal"
+        />
+        <ConfirmDeletionModal
+          heading={`Are you sure you want to delete this review?`}
+          loading={false}
+          onClose={() => setIsConfirmDeleteModalOpen(false)}
+          show={isConfirmDeleteModalOpen}
+          onDelete={async () => {
+            try {
+              console.log("Do delete");
+              // check data was returned
+
+              // hide modal
+              setIsConfirmDeleteModalOpen(false);
+            } catch (err) {
+              console.log(err);
+            }
+          }}
+          error={undefined}
+        />
+      </div>
+      <div className="space-y-6">
+        <MyWrapper>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="productId">Review ID</BasicInputLabel>
+            <DisabledInput name="reviewId" value={review.id.toString()} />
+          </div>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="productId">Product ID</BasicInputLabel>
+            <DisabledInput
+              name="productId"
+              value={review.productId.toString()}
+            />
+          </div>
+        </MyWrapper>
+        <MyWrapper>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="nickname">Nickname</BasicInputLabel>
+            <DisabledInput name="nickname" value={review.nickname} />
+          </div>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="publishedAt">
+              Published At
+            </BasicInputLabel>
+            <DisabledInput
+              name="publishedAt"
+              value={review.publishedAt ? review.publishedAt.toString() : "-"}
+            />
+          </div>
+        </MyWrapper>
+        <MyWrapper>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="createdAt">Created At</BasicInputLabel>
+            <DisabledInput
+              name="createdAt"
+              value={new Date(review.createdAt.toString()).toLocaleString()}
+            />
+          </div>
+          <div className="w-full">
+            <BasicInputLabel htmlFor="updatedAt">Updated At</BasicInputLabel>
+            <DisabledInput
+              name="updatedAt"
+              value={new Date(review.updatedAt.toString()).toLocaleString()}
+            />
+          </div>
+        </MyWrapper>
       </div>
 
       <div className="pt-4">
         <MyRatingSelect readonly initialRating={review.rating} />
       </div>
       <Formik
+        validationSchema={updateProductReviewValidationSchema}
         onSubmit={async (values: FormValues, { setSubmitting }) => {
           try {
             console.log("Submit form:", values);
@@ -52,27 +121,53 @@ export const EditProductReview = ({ review }: EditProductReviewProps) => {
           isPublished: review.isPublished,
         }}
       >
-        {({ values, errors, isSubmitting }) => {
+        {({ values, errors, isSubmitting, touched }) => {
           return (
             <Form>
-              <div className="space-y-2">
-                <BasicInputLabel htmlFor="name">Title</BasicInputLabel>
-                <Field
-                  className={`${inputClassNames} text-13`}
-                  id="title"
-                  name="title"
-                  type="text"
-                  as={BaseInput}
-                  label="title"
-                  value={values.title}
-                  placeholder="Title"
-                  autoComplete="off"
-                />
-                <ErrorMessage name="title">
-                  {(msg) => (
-                    <div className="pt-0 text-[11px] text-red-600">{msg}</div>
-                  )}
-                </ErrorMessage>
+              <div className="space-y-6">
+                <div className="space-y-2">
+                  <BasicInputLabel htmlFor="name">Title</BasicInputLabel>
+                  <Field
+                    className={`${inputClassNames} text-13`}
+                    id="title"
+                    name="title"
+                    type="text"
+                    as={BaseInput}
+                    label="title"
+                    hasError={errors.title && touched.title}
+                    value={values.title}
+                    placeholder="Title"
+                    autoComplete="off"
+                  />
+                  <ErrorMessage name="title">
+                    {(msg) => (
+                      <div className="pt-0 text-[11px] text-red-600">{msg}</div>
+                    )}
+                  </ErrorMessage>
+                </div>
+
+                <div className="space-y-2">
+                  <BasicInputLabel htmlFor="content">Content</BasicInputLabel>
+
+                  <Field
+                    className={`${inputClassNames} text-13 w-full`}
+                    id="content"
+                    name="content"
+                    as={CustomTextArea}
+                    rows={7}
+                    label="Content"
+                    hasError={errors.content && touched.content}
+                    currentValue={values.content}
+                    placeholder="Content"
+                    aria-label="Content"
+                    autoComplete="off"
+                  />
+                  <ErrorMessage name="content">
+                    {(msg) => (
+                      <div className="pt-2 text-[11px] text-red-600">{msg}</div>
+                    )}
+                  </ErrorMessage>
+                </div>
               </div>
 
               <div className="pt-4">
@@ -88,6 +183,20 @@ export const EditProductReview = ({ review }: EditProductReviewProps) => {
           );
         }}
       </Formik>
+    </div>
+  );
+};
+
+const MyWrapper: React.FC<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = ({ children, ...props }) => {
+  return (
+    <div
+      className={`space-y-4 md:flex md:items-center md:justify-between md:space-x-7 md:space-y-0 ${
+        props.className ? props.className : ""
+      }`}
+    >
+      {children}
     </div>
   );
 };
