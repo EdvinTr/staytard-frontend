e5ada86cf764ba9d2a38168cd46019cec7bbb5fe
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-02-28 16:33:16 +0100

    feat: front page done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { FadeInContainer } from "../components/global/FadeInContainer";
 import { MyContainer } from "../components/global/MyContainer";
 import { HeroImageSection } from "../components/pages/index/HeroImageSection";
@@ -15,7 +16,7 @@ interface HomePageProps {
   products: FindProductsQuery["products"];
 }
 const IndexPage: NextPage<HomePageProps> = ({ products }) => {
-  const hasProducts = products && products.items.length > 0;
+  const hasProducts = products && products.items?.length > 0;
   return (
     <FadeInContainer className="text-staytard-dark min-h-screen">
       <div className=" text-staytard-dark">
@@ -46,53 +47,111 @@ const IndexPage: NextPage<HomePageProps> = ({ products }) => {
                 );
               })}
           </div>
-          <div className="mx-auto max-w-[85rem] py-20 md:space-x-10 lg:flex 2xl:max-w-[95rem] 2xl:-space-x-10">
-            <div className="lg:w-7/12">
-              <Link href="/clothes/jeans">
-                <a>
-                  <Image
-                    src="/img/front-page/2208_denim_catpush_new_season.webp"
-                    width={800}
-                    height={980}
-                    objectFit="contain"
-                    className="w-full"
-                    alt="Man in white shirt and blue jeans"
-                  />
-                  <div className="pt-4 pb-8 text-center uppercase lg:pb-0 lg:pt-2 lg:text-left">
-                    <h4 className="text-xl font-semibold tracking-wide lg:text-2xl">
-                      Spring jeans
-                    </h4>
-                    <div className="inline-block border-b border-black pt-1 font-light">
-                      Shop here
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </div>
-            <div className="grid grid-cols-2 gap-4 md:gap-0 lg:w-5/12 2xl:gap-y-8">
-              {hasProducts &&
-                products.items.slice(4, 8).map((product, idx) => {
-                  return (
-                    <div
-                      key={idx}
-                      className="lg:h-52 lg:w-52 xl:h-60 xl:w-[16rem] 2xl:h-72 2xl:w-80"
-                    >
-                      <ProductCard
-                        isLoading={false}
-                        product={product as ProductItem}
-                      />
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
+          {hasProducts && (
+            <CallToActionProductCardList
+              href="/clothes/jeans"
+              image={{
+                src: "/img/front-page/2208_denim_catpush_new_season.webp",
+                alt: "Man in white shirt and blue jeans",
+                width: 800,
+                height: 980,
+              }}
+              className="py-20"
+              title="Spring jeans"
+              subtitle="Shop here"
+              products={products.items.slice(4, 8) as ProductItem[]}
+            />
+          )}
         </MyContainer>
-        <MyContainer className="space-y-12 py-20">
+        <MyContainer className="py-20">
           <ProductNewsSection />
+          {hasProducts && (
+            <CallToActionProductCardList
+              className="pt-16 pb-10"
+              href="/clothes"
+              image={{
+                src: "/img/front-page/top-selling.webp",
+                alt: "Multiple skewed images in one image of man in different clothes",
+                width: 900,
+                height: 1100,
+              }}
+              reversed
+              title="Selling most right now"
+              subtitle="Shop from the top list"
+              products={products.items.slice(8, 12) as ProductItem[]}
+            />
+          )}
           <ShopByBrandSection />
         </MyContainer>
       </div>
     </FadeInContainer>
+  );
+};
+
+interface CallToActionProductCardListProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  products: ProductItem[];
+  href: string;
+  image: {
+    src: string;
+    alt: string;
+    height: number;
+    width: number;
+  };
+  title: string;
+  subtitle: string;
+  reversed?: boolean;
+}
+const CallToActionProductCardList = ({
+  href,
+  image,
+  products,
+  subtitle,
+  title,
+  reversed,
+  className,
+}: CallToActionProductCardListProps) => {
+  return (
+    <div
+      className={`mx-auto max-w-[85rem] lg:flex 2xl:max-w-[95rem] ${
+        className ? className : ""
+      } ${reversed ? "lg:flex-row-reverse" : "md:space-x-10 2xl:-space-x-10"}`}
+    >
+      <div className={`lg:w-7/12 ${reversed ? "lg:ml-10" : ""}`}>
+        <Link href={href}>
+          <a>
+            <Image
+              src={image.src}
+              width={image.width}
+              height={image.height}
+              objectFit="contain"
+              className="w-full"
+              alt={image.alt}
+            />
+            <div className="pt-4 pb-8 text-center uppercase lg:pb-0 lg:pt-2 lg:text-left">
+              <h4 className="text-xl font-semibold tracking-wide lg:text-2xl">
+                {title}
+              </h4>
+              <div className="inline-block border-b border-black pt-1 font-light">
+                {subtitle}
+              </div>
+            </div>
+          </a>
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:gap-0 lg:w-5/12 2xl:gap-y-8">
+        {products.map((product, idx) => {
+          return (
+            <div
+              key={idx}
+              className="lg:h-52 lg:w-52 xl:h-60 xl:w-[16rem] 2xl:h-72 2xl:w-80"
+            >
+              <ProductCard isLoading={false} product={product as ProductItem} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
