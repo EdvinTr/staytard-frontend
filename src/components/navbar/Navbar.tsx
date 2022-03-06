427856e548cc368701854a6299a223125bc86284
Edvin Trönnberg <edvin.tronnberg@gmail.com>
2022-03-06 12:46:03 +0100

    add: netlify toml

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -21,7 +21,6 @@ import { MyCartIcon, MyUserIcon } from "../global/icons/Icons";
 import { MyContainer } from "../global/MyContainer";
 import { HoverMenu } from "./hover-menu/HoverMenu";
 import { MyPagesPopover } from "./my-pages-popover/MyPagesPopover";
-import { SearchModal } from "./SearchModal";
 
 const { deliveryTimeText, freeShippingText, rightOfReturnText } =
   Localized.page.index;
@@ -198,7 +197,6 @@ export const Navbar = () => {
               <></>
             )}
           </div>
-          <SearchModal onClose={() => setShowModal(false)} show={showModal} />
         </div>
       </div>
     </>
